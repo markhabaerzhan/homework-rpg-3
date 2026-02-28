5f01b13df297b9992fbd23efc7eefa1bb235633f
markhabaerzhan <markhaba.erzhankyzy@narxoz.kz>
2026-02-28 20:01:08 +0500

    Add EnemyCombatantAdapter implementation

--- src/com/narxoz/rpg/adapter/EnemyCombatantAdapter.java
+++ src/com/narxoz/rpg/adapter/EnemyCombatantAdapter.java
@@ -9,23 +9,18 @@ public class EnemyCombatantAdapter implements Combatant {
     public EnemyCombatantAdapter(Enemy enemy) {
         this.enemy = enemy;
     }
-
     @Override
     public String getName() {
         return enemy.getTitle();
     }
-
     @Override
     public int getAttackPower() {
-        // TODO: translate enemy damage to combat attack
         return enemy.getDamage();
     }
-
     @Override
     public void takeDamage(int amount) {
         enemy.applyDamage(amount);
     }
-
     @Override
     public boolean isAlive() {
         return !enemy.isDefeated();
